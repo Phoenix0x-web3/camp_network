a1e99e4a1153dd8896a3627c519b0a244218d484
saimondanya <backfire314@gmail.com>
2025-08-21 16:20:48 +0300

    update

--- modules/tasks/camp_tasks/scoreplay.py
+++ modules/tasks/camp_tasks/scoreplay.py
@@ -240,7 +240,7 @@ class Scoreplay(Base):
         elif data.get("success") is True:
             return True
         elif data.get("message") == "You have already claimed your daily reward. Please wait." and data.get("timeLeft"):
-            return True
+            return False
         elif data.get("message") == "Too many requests. Try again later.":
             return False
         elif data.get("success") is not True:
